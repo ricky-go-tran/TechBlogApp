3310f2a7c722adbfb0c38c805edf790624e20d45
Ricky <ricky.tran.goldenowl@gmail.com>
2023-07-02 21:32:20 +0700

    Init: article (model, controller, migration)

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_29_091534) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_02_063446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "publish"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_091534) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
