b1e837395105e5a2f4ef0d4aa9d96baa04845bad
Ricky <ricky.tran.goldenowl@gmail.com>
2023-07-05 17:56:32 +0700

    Add: CRUD of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,42 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user_id = 3
+    if @article.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    render root_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :publish)
+  end
 end
