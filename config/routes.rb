3c7ec4cd93f0acefd0043032483a83f2c4c740d4
Ricky <ricky.tran.goldenowl@gmail.com>
2023-07-05 09:20:01 +0700

    Update: name's user css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "articles#index"
-  resources :articles
+  #root "articles#index"
+  #resources :articles
   devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
