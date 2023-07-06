06897b3054248c48cfc2320b3230669be7721d9c
Ricky <ricky.tran.goldenowl@gmail.com>
2023-07-06 17:17:51 +0700

    Update: apply path into button

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
     end
   end
   devise_for :users
-  # get 'authors', to: 'users#author'
-  # get 'authors/:id', to: 'users#article_by_author'
-  # get 'manager', to: 'users#account_manage'
+  get 'authors', to: 'users#author'
+  get 'authors/:id', to: 'users#article_by_author'
+  get 'manager', to: 'users#account_manage'
   # get 'manager/article', to: 'articles#manage'
 end
